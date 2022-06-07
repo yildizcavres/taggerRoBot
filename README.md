45f7f9ddf78e9030339c1edbdd22a9ac2f7a4061
EfeApakDev <93475651+EfeApakDev@users.noreply.github.com>
2022-06-07 20:08:09 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# taggerRoBot
+### 🚀 Heroku'ya dağıtın
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Kenan20222/ASOTagger)
---
+++ Tagger.py
@@ -0,0 +1,297 @@
+import os, logging, asyncio
+from telethon import Button
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.types import ChannelParticipantsAdmins
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(name)s - [%(levelname)s] - %(message)s'
+)
+LOGGER = logging.getLogger(__name__)
+
+api_id = int(os.environ.get("APP_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("TOKEN")
+client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+
+emoji_calisan = []
+
+anlik_calisan = []
+
+tekli_calisan = []
+
+@client.on(events.NewMessage(pattern='^(?i)/dur'))
+async def cancel(event):
+  global emoji_calisan
+  emoji_calisan.remove(event.chat_id)
+
+
+
+@client.on(events.NewMessage(pattern="^/start$"))
+async def start(event):
+  await event.reply("** Bozkurt Tagger Bot **\n ile Grupunuzdakı Bütün kullanıcılara Etiket Atabilirim \nKomutlar için =======> /komut yazın**",
+                    buttons=(
+                   
+		      [Button.url('Beni gruba ekle ➕', 'https://t.me/bozkurtaggerbot?startgroup=a')],
+                      [Button.url('Support✈️', 'https://t.me/muhabbetofkings')],
+                      [Button.url('Resmi kanal 📣', 'https://https://t.me/CyberTurkish')],
+		      [Button.url('Sahibim👨🏻‍💻', 'https://t.me/bozkurt20')],
+                    ),
+                    link_preview=False
+                   )
+@client.on(events.NewMessage(pattern="^/komut$"))
+async def help(event):
+  helptext = "**🐺🇹🇷  Bozkurt Tagger Bot Komutları**\n\n**/tag <sepep> - 5-li Etiket Atar**\n\n**/etag <sebep> - Emoji ile etiketler**\n\n**/tektag sebep - kullanıcıları Tek Tek Etiketler**\n\n**/adminler sebep - Yöneticileri Tek Tek Tag Eder**\n\n**/start - botu başlatır**"
+  await event.reply(helptext,
+                    buttons=(
+                      [Button.url('Beni Gruba Ekle➕', 'https://t.me/bozkurtaggerbot?startgroup=a')],
+                      [Button.url('Support✈️', 'https://t.me/muhabbetofkings')],
+                      [Button.url('Resmi kanal 🔖', 'https://t.me/CyberTurkish')],
+		      [Button.url('Sahibim🧑‍🔧', 'https://t.me/bozkurt20')],
+                    ),
+                    link_preview=False
+                   )
+	
+@client.on(events.NewMessage(pattern="^/reklam$"))
+async def help(event):
+  helptext = "**Çok özellikleri Etiket Botu Bulmaya Çalışan Grup Sahibleri @bozkurtaggerbot Size Göre:\n\n📌 5-li etiket\n📌 Emoji etiket\n📌 Tekli Etiket\n📌 Yöneticileri etiketleme\n📌\n\n Böyle Çok özellikli @bozkurtaggerbot'u grubunuza yönetici olarak ekleyip rahatlıkla üyelere , etiket ata bilirsiz **"
+  await event.reply(helptext,
+                    buttons=(
+                      [Button.url('Beni gruba ekle➕', 'https://t.me/bozkurtaggerbot?startgroup=a' )],
+                    ),
+                    link_preview=False
+                   )
+	
+	
+
+@client.on(events.NewMessage(pattern='^(?i)/dur'))
+async def dur(event):
+  global emoji_calisan
+  emoji_calisan.remove(event.chat_id)
+
+
+emoji = " ❤️ 🧡 💛 💚 💙 💜 🖤 🤍 🤎 🙂 🙃 😉 😌 😍 🥰 😘 😗 😙 😚 😋 😛 😝 😜 🤪 🤨 🧐 🤓 😎 🤩 🥳 😏 😒 " \
+        "😞 😔 😟 😕 🙁 😣 😖 😫 😩 🥺 😢 😭 😤 😠 😡  🤯 😳 🥵 🥶 😱 😨 😰 😥 😓 🤗 🤔 🤭 🤫 🤥 😶 😐 😑 😬 🙄 " \
+        "😯 😦 😧 😮 😲 🥱 😴 🤤 😪 😵 🤐 🥴 🤢 🤮 🤧 😷 🤒 🤕 🤑 🤠 😈 👿 👹 👺 🤡  👻 💀 👽 👾 🤖 🎃 😺 😸 😹 " \
+        "😻 😼 😽 🙀 😿🇹🇷🐺 🦄 😾".split(" ")
+
+
+@client.on(events.NewMessage(pattern="^/etag ?(.*)"))
+async def mentionall(event):
+  global emoji_calisan
+  if event.is_private:
+    return await event.respond("**Bu komutu gruplar ve kanallar için geçerli❗**")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond("**Bu komutu sadece yöneticiler kullanabilir〽️**")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("**Geçmiş mesajlar için etiket edemem**")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("etiketlemek için sebep yok🙁 @bozkurt20 🇹🇷")
+  else:
+    return await event.respond("**Etikete Başlamak için sebep yazın @bozkurt20 ..!**🇹🇷")
+  
+  if mode == "text_on_cmd":
+    emoji_calisan.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{random.choice(emoji)}](tg://user?id={usr.id}) "
+      if event.chat_id not in emoji_calisan:
+        await event.respond("** durdum🌹 @bozkurtaggerbot 🐺🇹🇷**")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+        
+  
+  if mode == "text_on_reply":
+    emoji_calisan.append(event.chat_id)
+ 
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{random.choice(emoji)}](tg://user?id={usr.id}) "
+      if event.chat_id not in emoji_calisan:
+        await event.respond("durdum🙁 @bozkurt20 \n\n**Burda sizin reklamınız ola bilər @bozkurtgamebot**🐺🇹🇷")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+
+
+@client.on(events.NewMessage(pattern='^(?i)/dur'))
+async def cancel(event):
+  global emoji_calisan
+  emoji_calisan.remove(event.chat_id)
+
+
+@client.on(events.NewMessage(pattern="^/tag ?(.*)"))
+async def mentionall(event):
+  global anlik_calisan
+  if event.is_private:
+    return await event.respond("Bu komutu gruplar ve kanallar için geçerli❗️**")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond("**Bu komutu sadece yöneticiler kullanabilir〽️**")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("Önceki Mesajlara Cevap Vermeyin")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("Başlamak için sebep yokdu🙁 @bozkurtgamebot")
+  else:
+    return await event.respond("Işleme başlamak için sebep yokdu🙁 @bozkurtaggerbot ")
+  
+  if mode == "text_on_cmd":
+    anlik_calisan.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"👤 - [{usr.first_name}](tg://user?id={usr.id}) \n"
+      if event.chat_id not in anlik_calisan:
+        await event.respond("Durdum🌹 @bozkurtaggerbot\n\n**Burda sizin reklamınız ola bilir @bozkurtaggerbot **🐺🇹🇷🇦🇿")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+        
+  
+  if mode == "text_on_reply":
+    anlik_calisan.append(event.chat_id)
+ 
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"👤 - [{usr.first_name}](tg://user?id={usr.id}) \n"
+      if event.chat_id not in anlik_calisan:
+        await event.respond("durdum🌹 @bozkurtaggerbot 🐺🇹🇷")
+        return
+      if usrnum == 5:
+        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+
+@client.on(events.NewMessage(pattern='^(?i)/dur'))
+async def cancel(event):
+  global anlik_calisan
+  anlik_calisan.remove(event.chat_id)
+	
+
+@client.on(events.NewMessage(pattern="^/tektag ?(.*)"))
+async def mentionall(event):
+  global tekli_calisan
+  if event.is_private:
+    return await event.respond("**Bu komutu gruplar ve kanallar için geçerli❗️**")
+  
+  admins = []
+  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond("**Bu komutu sadece yöneticiler kullanabilir〽**")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg == None:
+        return await event.respond("**önceki mesajı etiketleyemem*")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("Başlamaq üçün Səbəb Yazın @bozkurtaggerbot 🇹🇷")
+  else:
+    return await event.respond("**işleme başlamam için sebep yazın..**🇹🇷")
+  
+  if mode == "text_on_cmd":
+    tekli_calisan.append(event.chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"**👤 - [{usr.first_name}](tg://user?id={usr.id}) \n**"
+      if event.chat_id not in tekli_calisan:
+        await event.respond("**Durdum🌹 @bozkurtaggerbot\n\n**Burda sizin reklamınız ola bilər @bozkurtaggerbot **🇹🇷🐺****")
+        return
+      if usrnum == 1:
+        await client.send_message(event.chat_id, f"{usrtxt} {msg}")
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+        
+  
+  if mode == "text_on_reply":
+    tekli_calisan.append(event.chat_id)
+ 
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"👤 - [{usr.first_name}](tg://user?id={usr.id}) \n"
+      if event.chat_id not in tekli_calisan:
+        await event.respond("Durdum🌹 @bozkurtaggerbot\n\n**Burda sizin reklamınız ola bilir @bozkurtaggerbot **🇹🇷**")
+        return
+      if usrnum == 1:
+        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
+        await asyncio.sleep(2)
+        usrnum = 0
+        usrtxt = ""
+
+@client.on(events.NewMessage(pattern='^(?i)/dur'))
+async def cancel(event):
+  global tekli_calisan
+  tekli_calisan.remove(event.chat_id)
+	
+
+
+@client.on(events.NewMessage(pattern="^/adminler ?(.*)"))
+async def mentionall(tagadmin):
+
+	if tagadmin.pattern_match.group(1):
+		seasons = tagadmin.pattern_match.group(1)
+	else:
+		seasons = ""
+
+	chat = await tagadmin.get_input_chat()
+	a_=0
+	await tagadmin.delete()
+	async for i in client.iter_participants(chat, filter=cp):
+		if a_ == 500:
+			break
+		a_+=5
+		await tagadmin.client.send_message(tagadmin.chat_id, "**[{}](tg://user?id={}) {}**".format(i.first_name, i.id, seasons))
+		sleep(0.5)
+
+
+print(">> Bot çalışıyor🥳🥳<<")
+client.run_until_disconnected()
